d20f873bfd50551a1d3df0659581cc33a5a8bbce
maitte <maitena_escalada@hotmail.com>
2022-04-15 23:30:55 -0300

    Add errorMensaje y textoVacioMensaje

--- app.js
+++ app.js
@@ -5,55 +5,26 @@ var botonLimpiar = document.querySelector("#boton-limpiar");
 var textAreaCifrador = document.querySelector("#text-cifrador");
 var textAreaResultado = document.querySelector("#text-resultado");
 
-function validarTexto(texto){
-    var filtro = 'abcdefghijklmnñopqrstuvwxyz ';
-        
-        for (var i = 0; i < texto.length; i++)
-           if (esCaracterValido(filtro, texto, i)) {
-                return false;
-           }
-            
-        return true;
-}
-
-function esCaracterValido(texto, letra, i){
-    return texto.indexOf(letra.charAt(i)) == -1;
-}
-
 function encriptar(texto){
     var textoEncriptado = "";
     
-    if(texto.length == 0){
-        alert("No has ingresado nada");
-    }
-    if(validarTexto(texto)) {
-        textoEncriptado = texto.replace(/e/g, "enter");
-        textoEncriptado = textoEncriptado.replace(/i/g, "imes");
-        textoEncriptado = textoEncriptado.replace(/a/g, "ai");
-        textoEncriptado = textoEncriptado.replace(/o/g, "ober");
-        textoEncriptado = textoEncriptado.replace(/u/g, "ufat");
-    } else {
-        alert("Sólo se admiten letras minúsculas y sin acentos");
-    }
+    textoEncriptado = texto.replace(/e/g, "enter");
+    textoEncriptado = textoEncriptado.replace(/i/g, "imes");
+    textoEncriptado = textoEncriptado.replace(/a/g, "ai");
+    textoEncriptado = textoEncriptado.replace(/o/g, "ober");
+    textoEncriptado = textoEncriptado.replace(/u/g, "ufat");
+    
     return textoEncriptado;
 }
 
 function desencriptar(texto){
     var textoDesencriptado = "";
     
-    if(texto.length == 0){
-        alert("No has ingresado nada");
-    }
-    if(validarTexto(texto)) {
     textoDesencriptado = texto.replace(/enter/g, "e");
     textoDesencriptado = textoDesencriptado.replace(/imes/g, "i");
     textoDesencriptado = textoDesencriptado.replace(/ai/g, "a");
     textoDesencriptado = textoDesencriptado.replace(/ober/g, "o");
     textoDesencriptado = textoDesencriptado.replace(/ufat/g, "u");
-    }
-    else {
-        alert("Sólo se admiten letras minúsculas y sin acentos");
-    }
 
     return textoDesencriptado;
 }
@@ -66,14 +37,80 @@ function copiarTexto(){
     navigator.clipboard.writeText(copiarTexto.value);
 }
 
+function validarTexto(texto){
+    var filtro = 'abcdefghijklmnñopqrstuvwxyz ';
+        
+    for (var i = 0; i < texto.length; i++) {
+        if (!esCaracterValido(filtro, texto, i)) {
+            return false;
+        }    
+    }
+    return true;
+}
+
+function esCaracterValido(texto, letra, i){
+    return texto.indexOf(letra.charAt(i)) != -1;
+}
+
+function esVacio(texto){
+    return texto.length == 0;
+}
+
+function errorMensaje(){
+    var error = document.querySelector("#error");
+    error.textContent = "Error. Ha ingresado un carácter inválido.";
+}
+
+function textoVacioMensaje(){
+    var mensaje = document.querySelector("#error");
+    mensaje.textContent = "No ha ingresado ningún texto.";
+}
+
 //eventos botones
 botonEncriptar.addEventListener("click", function() {
-    textAreaResultado.value = encriptar(textAreaCifrador.value);
+    var input = textAreaCifrador.value;
+    
+    var error = !validarTexto(input);
+    var vacio = esVacio(input);
+    
+    if(vacio){
+        textoVacioMensaje();
+        return;
+    }
+    
+    if(error){
+        errorMensaje();
+        return;
+    }
+    
+    var errorReset = document.querySelector("#error");
+    errorReset.innerHTML = "";
+    
+    textAreaResultado.value = encriptar(input);
     textAreaCifrador.value = "";
+    
 });
 
 botonDesencriptar.addEventListener("click", function() {
-    textAreaResultado.value = desencriptar(textAreaCifrador.value);
+    var input = textAreaCifrador.value;
+    
+    var error = !validarTexto(input);
+    var vacio = esVacio(input);
+    
+    if(vacio){
+        textoVacioMensaje();
+        return;
+    }
+    
+    if(error){
+        errorMensaje();
+        return;
+    }
+    
+    var errorReset = document.querySelector("#error");
+    errorReset.innerHTML = "";
+
+    textAreaResultado.value = desencriptar(input);
     textAreaCifrador.value = "";
 });
 
